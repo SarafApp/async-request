3aba5a870afd4c1df7ee18588603b14fc4633de2
Amirhossein Adibinia <amir.adibinia@gmail.com>
2025-07-18 13:26:01 +0330

    feat: add coockies support in CurlDumpTrait

--- src/CurlDumpTrait.php
+++ src/CurlDumpTrait.php
@@ -5,6 +5,38 @@ namespace Saraf;
 trait CurlDumpTrait
 {
     /**
+     * Additional cookies to include in curl command
+     */
+    private array $curlCookies = [];
+
+    /**
+     * Add a cookie to be included in curl commands
+     */
+    public function addCookie(string $name, string $value): self
+    {
+        $this->curlCookies[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * Remove a cookie from curl commands
+     */
+    public function removeCookie(string $name): self
+    {
+        unset($this->curlCookies[$name]);
+        return $this;
+    }
+
+    /**
+     * Clear all cookies from curl commands
+     */
+    public function clearCookies(): self
+    {
+        $this->curlCookies = [];
+        return $this;
+    }
+
+    /**
      * Generate a curl command for the given request parameters
      * 
      * @param string $method HTTP method (GET, POST, PUT, etc.)
@@ -39,12 +71,30 @@ trait CurlDumpTrait
         $browserHeaders = $this->extractHeadersFromBrowser();
         $allHeaders = array_merge($browserHeaders, $headers);
         
-        // Add headers
+        // Separate cookies from other headers
+        $cookieHeaders = [];
+        $otherHeaders = [];
+        
         foreach ($allHeaders as $key => $value) {
+            if (strtolower($key) === 'cookie') {
+                $cookieHeaders[$key] = $value;
+            } else {
+                $otherHeaders[$key] = $value;
+            }
+        }
+        
+        // Add non-cookie headers
+        foreach ($otherHeaders as $key => $value) {
             if (is_array($value)) {
                 $value = implode(', ', $value);
             }
             $curl .= " -H " . escapeshellarg($key . ": " . $value);
+        }
+        
+        // Add cookies (both from headers and manually added)
+        $cookies = $this->extractCookiesFromHeaders($allHeaders);
+        if (!empty($cookies)) {
+            $curl .= " -H " . escapeshellarg("Cookie: " . $cookies);
         }
         
         // Add body if present
@@ -81,11 +131,37 @@ trait CurlDumpTrait
     {
         try {
             $reflection = new \ReflectionClass($this->browser);
-            $headersProperty = $reflection->getProperty('headers');
+            $headersProperty = $reflection->getProperty('defaultHeaders');
             $headersProperty->setAccessible(true);
             return $headersProperty->getValue($this->browser) ?: [];
         } catch (\Exception $e) {
             return [];
         }
+    }
+    
+    /**
+     * Extract cookies from headers and format them for curl
+     */
+    private function extractCookiesFromHeaders(array $headers): string
+    {
+        $cookies = [];
+        
+        // Look for Cookie header in request headers
+        foreach ($headers as $key => $value) {
+            if (strtolower($key) === 'cookie') {
+                if (is_array($value)) {
+                    $cookies = array_merge($cookies, $value);
+                } else {
+                    $cookies[] = $value;
+                }
+            }
+        }
+        
+        // Add manually added cookies
+        foreach ($this->curlCookies as $name => $value) {
+            $cookies[] = $name . '=' . $value;
+        }
+        
+        return implode('; ', $cookies);
     }
 } 
